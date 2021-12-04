9b4a6f5d9b42751ba26b448bb2d7646bab19b237
ChijiokeOrabueze <valentineorabueze@gmail.com>
2021-12-03 16:18:53 -0800

    Update Log_Image_Classification_With_Logistic_Regression.py

--- Log_Image_Classification_With_Logistic_Regression.py
+++ Log_Image_Classification_With_Logistic_Regression.py
@@ -1,1 +1,14 @@
-print('stra')
+import numpy as np
+import os
+import h5py
+import matplotlib.pyplot as plt
+from PIL import Image
+import copy, time
+
+
+
+train_data = h5py.File('train_log_150.h5', 'r')
+train_set_x_orig = np.array(train_data['Train_X'])
+train_set_y = np.array(train_data['Train_Y'], dtype = int)
+data_classes = np.array(train_data['classes'])
+train_data.close()
